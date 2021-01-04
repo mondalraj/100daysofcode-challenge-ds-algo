9b5f2d13fc08601f1b5d22a3c02c5d311d050f55
mondalraj <mondal021002rajib@gmail.com>
2021-01-04 20:46:53 +0530

    initial commit

---
+++ prime_number.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num;
+    int a;
+    cin >> num;
+
+    for (a = 2; a < num; a++)
+    {
+        if (num % a == 0)
+        {
+            cout << "Not Prime" << endl;
+            break;
+        }
+    }
+    if (num == a)
+    {
+        cout << "Prime" << endl;
+    }
+}
