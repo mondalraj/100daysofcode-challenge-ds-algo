1947b7291a4bbf819c4c2d288da2b8dd70dd9792
mondalraj <mondal021002rajib@gmail.com>
2021-01-08 17:59:50 +0530

    Day 5 updated

---
+++ armstrong_number.cpp
@@ -0,0 +1,32 @@
+// WAP to check whether a number is Armstrong or not.
+
+#include <iostream>
+using namespace std;
+#include <math.h>
+
+int main()
+{
+    int num;
+    cout << "Enter a number";
+    cin << num;
+
+    int sum = 0;
+    int n = num;
+
+    while (n > 0)
+    {
+        int rem = n % 10;
+        n = n / 10;
+        sum = sum + rem * rem * rem;
+    }
+    if (sum == num)
+    {
+        cout << num << " is an Armstrong number";
+    }
+    else
+    {
+        cout << num << " is not an Armstrong number";
+    }
+
+    return 0;
+}
